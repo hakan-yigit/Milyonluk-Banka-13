2dc285363a4a10d308076d3585f7d0a9364c943c
hakan-yigit <@hakan-yigit>
2024-03-05 20:09:59 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,114 @@
+import { useState } from "react";
+import "./styles.css";
+
+import React from "react";
+import Message from "./components/Message";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+export default function App() {
+  const passCode = "1001";
+
+  const [userInput, setUserInput] = useState({
+    charOne: "",
+    charTwo: "",
+    charThree: "",
+    charFour: "",
+  });
+
+  const [verified, setVerified] = useState(undefined);
+
+
+  const handleChange = (e) => {
+    const inputName = e.target.name;
+    const value = e.target.value;
+    setUserInput({...userInput,[inputName] : value});
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const userPassCode = `${userInput.charOne}${userInput.charTwo}${userInput.charThree}${userInput.charFour}`
+    if(userPassCode === passCode){
+      setVerified(true);
+    }else{
+      setVerified(false);
+    }
+    setUserInput({
+      charOne: "",
+      charTwo: "",
+      charThree: "",
+      charFour: ""
+    });
+  };
+
+
+
+  return (
+    <div className="wrapper">
+      <Header />
+
+      <form>
+        <Message status={verified} />
+
+        <div>
+          <input 
+          required 
+          type="password" 
+          name="charOne" 
+          maxLength="1" 
+          onChange={handleChange}
+          value={userInput.charOne}
+          />
+
+          <input 
+          required 
+          type="password" 
+          name="charTwo" 
+          maxLength="1" 
+          onChange={handleChange}
+          value={userInput.charTwo}
+          />
+
+          <input 
+          required 
+          type="password"
+          name="charThree"
+          maxLength="1" 
+          onChange={handleChange}
+          value={userInput.charThree}
+          />
+
+          <input 
+          required 
+          type="password" 
+          name="charFour" 
+          maxLength="1" 
+          onChange={handleChange}
+          value={userInput.charFour}
+          />
+        </div>
+
+        <button disabled={verified} onClick={handleSubmit}>Gönder</button>
+      </form>
+
+      <Footer />
+    </div>
+  );
+}
+
+
+
+
+
+  /* Challenge
+
+	Doğrulama kodu formu henüz kullanıcının girdisini kontrol etmiyor. Sizin göreviniz aşağıdaki gibi kurulumu tamamlamaktır: 
+	
+		1. Kullanıcı parola input'larından birine bir karakter yazdığında, userInput state nesnesinin ilgili özelliği, diğer özellikler korunarak güncellenmelidir. Özellik adlarının ve girdilerin adlarının birbiriyle eşleştiğine dikkat edin (charOne, charTwo, vb.)
+		   
+		2. Kullanıcı gönder butonuna tıkladığında, bir gönderme işleme fonksiyonu sayfanın yenilenmesini engellemeli ve userInput'ta saklanan dört karakterin kombinasyonunun passCode değerine eşit olup olmadığını kontrol etmelidir (yukarıdaki 7. satırda bildirilmiştir).
+		   
+		3. Eşleşiyorlarsa, verified state değeri true olarak ayarlanmalıdır. Aksi takdirde false olarak ayarlanmalıdır. 
+		   
+		4. Kodunuz mümkün olduğunca DRY olmalıdır
+*/
